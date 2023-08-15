11ac4a93ffdd9a95ecd30881a0f4918fc986a69d
Esra Fokker <esra.fokker@finalist.nl>
2023-08-15 14:58:18 +0200

    test tls

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+use std::{
+    io::{Read, Write},
+    marker::PhantomData,
+    net::{SocketAddr, TcpStream},
+    pin::{self, pin},
+    task::Poll,
+};
+
 use axum::{
     routing::{get, MethodFilter, MethodRouter},
     Router,
 };
 use clap::Parser;
+use futures::{AsyncRead, AsyncReadExt, FutureExt};
+use http::header::CONTENT_SECURITY_POLICY;
+use hyper::server::accept::Accept;
 use notify::Watcher;
+use rustls::ServerConnection;
+use tokio::{io::Interest, net::TcpListener};
+use tokio_stream::{wrappers::TcpListenerStream, Stream, StreamExt};
 use tracing::{debug, error, info, Level};
 use webpages::gitea_handler;
-
 //mod api;
 mod cli;
 mod db;
@@ -145,6 +158,36 @@ async fn main() {
             state.args.server_host.clone(),
             state.args.https_port.clone(),
         );
+        let tls_server_config = Arc::new(
+            rustls::ServerConfig::builder()
+                .with_safe_defaults()
+                .with_no_client_auth()
+                .with_single_cert(
+                    load_certificates_from_pem(&state.args.ssl_certificate).unwrap(),
+                    load_private_key_from_file(&state.args.ssl_key).unwrap(),
+                )
+                .unwrap(),
+        );
+
+        let mut stream = TlsStream::new(
+            &tls_server_config,
+            tokio::net::TcpListener::bind(https)
+                .await
+                .unwrap()
+                .accept()
+                .await
+                .unwrap()
+                .0,
+        );
+        let mut res = Vec::new();
+        stream.read_to_end(&mut res).await;
+        println!("{}", String::from_utf8_lossy(res.as_slice()));
+
+        //let tls_acceptor = TlsAcceptor::new(https, &tls_server_config).await;
+        // let acceptor =
+        //         .map(|e| {});
+        // let stream = tok
+
         /*
         let https_server = warp::serve(req)
             .tls()
@@ -160,6 +203,93 @@ async fn main() {
         let http_server = warp::serve(static_path.or(redirect)).bind(http);
         tokio::spawn(https_server);
         http_server.await;*/
+    }
+}
+
+struct TlsAcceptor {}
+
+impl TlsAcceptor {
+    async fn new(socket_addr: SocketAddr, config: &Arc<rustls::ServerConfig>) -> Self {
+        Self {}
+    }
+}
+
+struct TlsStream {
+    rustls_con: Arc<tokio::sync::Mutex<ServerConnection>>,
+    task: tokio::task::JoinHandle<()>,
+    read_waker_sender: tokio::sync::mpsc::Sender<std::task::Waker>,
+    write_waker_sender: tokio::sync::mpsc::Sender<std::task::Waker>,
+}
+
+impl TlsStream {
+    fn new(config: &Arc<rustls::ServerConfig>, mut con: tokio::net::TcpStream) -> Self {
+        let (read_sender, mut read_reciever) = tokio::sync::mpsc::channel(16);
+        let (write_sender, mut write_reciever) = tokio::sync::mpsc::channel(16);
+        let rustls_con = Arc::new(tokio::sync::Mutex::new(
+            ServerConnection::new(config.clone()).unwrap(),
+        ));
+        let sec_con = rustls_con.clone();
+        Self {
+            task: tokio::spawn(async move {
+                let mut read_wakers: Vec<std::task::Waker> = vec![];
+                let mut write_wakers: Vec<std::task::Waker> = vec![];
+                let mut buf = vec![];
+                loop {
+                    tokio::select! {
+                        Some(w) = read_reciever.recv() => {
+                            read_wakers.push(w);
+                        },
+                        Some(v) = write_reciever.recv() => {
+                            write_wakers.push(v);
+                        },
+                        Ok(_) = con.readable() => {
+                            con.try_read_buf(&mut buf).unwrap();
+                            rustls_con.lock().await.read_tls(&mut &buf[..]).unwrap();
+                            buf.clear();
+                            rustls_con.lock().await.process_new_packets().unwrap();
+                            if !rustls_con.lock().await.wants_read() {
+                                for i in read_wakers.iter() {
+                                    i.wake_by_ref();
+                                }
+                                read_wakers.clear();
+                            }
+                        },
+                        Ok(_) = con.writable() => {
+                            rustls_con.lock().await.write_tls(&mut buf).unwrap();
+                            tokio::io::AsyncWriteExt::write(&mut con, &buf).await.unwrap();
+                            buf.clear();
+                            if !rustls_con.lock().await.wants_write() {
+                                for i in write_wakers.iter() {
+                                    i.wake_by_ref();
+                                }
+                                write_wakers.clear();
+                            }
+                        }
+                    }
+                }
+            }),
+            rustls_con: sec_con,
+            read_waker_sender: read_sender,
+            write_waker_sender: write_sender,
+        }
+    }
+}
+
+impl AsyncRead for TlsStream {
+    fn poll_read(
+        self: pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut [u8],
+    ) -> std::task::Poll<std::io::Result<usize>> {
+        let mut lock = self.rustls_con.blocking_lock();
+        if !lock.wants_read() {
+            Poll::Ready(lock.reader().read(buf))
+        } else {
+            match self.read_waker_sender.blocking_send(cx.waker().clone()) {
+                Ok(_) => Poll::Pending,
+                Err(_) => Poll::Ready(Err(std::io::ErrorKind::ConnectionAborted.into())),
+            }
+        }
     }
 }
 
@@ -185,4 +315,27 @@ fn signal_handler(state: Arc<State>) -> notify::INotifyWatcher {
         notify::RecursiveMode::Recursive,
     );
     watcher
+}
+
+/// taken from [rustls docs](https://docs.rs/rustls/latest/rustls/struct.PrivateKey.html#Examples)
+fn load_private_key_from_file(
+    path: &str,
+) -> Result<rustls::PrivateKey, Box<dyn std::error::Error>> {
+    let file = std::fs::File::open(&path)?;
+    let mut reader = std::io::BufReader::new(file);
+    let mut keys = rustls_pemfile::pkcs8_private_keys(&mut reader)?;
+
+    match keys.len() {
+        0 => Err(format!("No PKCS8-encoded private key found in {path}").into()),
+        1 => Ok(rustls::PrivateKey(keys.remove(0))),
+        _ => Err(format!("More than one PKCS8-encoded private key found in {path}").into()),
+    }
+}
+/// taken from [rustls docs](https://docs.rs/rustls/latest/rustls/struct.Certificate.html#Examples)
+fn load_certificates_from_pem(path: &str) -> std::io::Result<Vec<rustls::Certificate>> {
+    let file = std::fs::File::open(path)?;
+    let mut reader = std::io::BufReader::new(file);
+    let certs = rustls_pemfile::certs(&mut reader)?;
+
+    Ok(certs.into_iter().map(rustls::Certificate).collect())
 }
