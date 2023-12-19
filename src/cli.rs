a6b6e46cab2c9af68cd34a691cad9df2124597d7
Esfo <esra.fokker@finalist.nl>
2023-12-19 22:32:33 +0100

    cli cleanup now nginx handles ssl

--- src/cli.rs
+++ src/cli.rs
@@ -5,9 +5,6 @@ pub struct Cli {
     #[arg(long, default_value_t = 80)]
     pub http_port: u16,
 
-    #[arg(long, default_value_t = 443)]
-    pub https_port: u16,
-
     #[arg(long, default_value_t = [0, 0, 0, 0].into())]
     pub server_host: std::net::IpAddr,
 
@@ -34,14 +31,6 @@ pub struct Cli {
 
     #[arg(long, default_value_t = String::from("templates"))]
     pub template_dir: String,
-
-    #[arg(long, default_value_t = String::from("/etc/letsencrypt/live/esfokk.nl/fullchain.pem"))]
-    pub ssl_certificate: String,
-    #[arg(long, default_value_t = String::from("/etc/letsencrypt/live/esfokk.nl/privkey.pem"))]
-    pub ssl_key: String,
-
-    #[arg(long, default_value_t = String::from("/php"))]
-    pub php_root: String,
 
     #[arg(long)]
     /// enable dev mode
